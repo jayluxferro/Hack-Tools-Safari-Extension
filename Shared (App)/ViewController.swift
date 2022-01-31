009aa2f7715306ac7092fdbe37a37b9b22644c55
Jay <jayluxferro@sperixlabs.org>
2022-01-31 23:21:18 +0000

    release 0.4.0

--- Shared (App)/ViewController.swift
+++ Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "org.sperixlabs.Hack-Tools.Extension"
+let extensionBundleIdentifier = "org.sperixlabs.HackTools.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
